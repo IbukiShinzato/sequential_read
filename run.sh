ad5da069b07111976a8fd346554cb877416cd645
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2026-04-13 19:41:53 +0900

    feat: 単位を変数として扱う処理に変更、実験用ファイルの自動作成、古いバイナリをリセット

--- run.sh
+++ run.sh
@@ -1,23 +1,38 @@
 ##!/bin/zsh
 
-echo "Building binary..."
-cargo build --release
-BINARY="./target/release/sequential_read"
+FILE="testfile"
+if [ ! -f "$FILE" ]; then
+    echo "Creating 1GB test file..."
+    dd if=/dev/zero of=${FILE} bs=1M count=1024 > /dev/null 2>&1
+fi
 
-SIZES=(1024 4096 65536 1048576 1073741824)
+KB=1024
+MB=$(( 1024 * KB ))
+GB=$(( 1024 * MB ))
+
+SIZES=(
+  $(( 1 * KB ))
+  $(( 4 * KB ))
+  $(( 64 * KB ))
+  $(( 1 * MB ))
+)
+
+echo "Building binary..."
+cargo clean -q
+cargo build --release -q
+BINARY="./target/release/sequential_read"
 
 echo "Starting benchmarks..."
 echo "------------------------------------------------"
 
 for sz in "${SIZES[@]}"; do
-    echo -n "Mode: read, Buffer Size: $sz bytes "
+    echo "Mode: read, Buffer Size: $sz bytes "
     sudo purge
     (time $BINARY read $sz) > /dev/null
     echo "------------------------------------------------"
 done
 
-echo -n "Mode: mmap "
+echo "Mode: mmap "
 sudo purge
 (time $BINARY mmap) > /dev/null
 echo "------------------------------------------------"
-
